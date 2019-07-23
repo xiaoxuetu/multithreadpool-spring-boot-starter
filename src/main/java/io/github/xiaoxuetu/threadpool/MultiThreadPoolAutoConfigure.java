49e0b355c8f09f9895c6892c1c8e07b50caeae7a
xiaoxuetu <hjj20040849@qq.com>
2019-07-24 01:20:47 +0800

    first commit

---
+++ src/main/java/io/github/xiaoxuetu/threadpool/MultiThreadPoolAutoConfigure.java
@@ -0,0 +1,81 @@
+package io.github.xiaoxuetu.threadpool;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.PropertyMapper;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@EnableConfigurationProperties(MultiThreadPoolProperties.class)
+public class MultiThreadPoolAutoConfigure {
+
+    /**
+     * 线程池缓存
+     */
+    private Map<String, ThreadPoolTaskExecutor> threadPoolTaskExecutorCache = new HashMap<>();
+
+    /**
+     * 线程池
+     */
+    private MultiThreadPoolProperties multiThreadPoolProperties;
+
+    private ApplicationContext applicationContext;
+
+    private final ObjectProvider<TaskDecorator> taskDecorator;
+
+    public MultiThreadPoolAutoConfigure(MultiThreadPoolProperties multiThreadPoolProperties,
+                                        ApplicationContext applicationContext,
+                                        ObjectProvider<TaskDecorator> taskDecorator) {
+        this.multiThreadPoolProperties = multiThreadPoolProperties;
+        this.applicationContext = applicationContext;
+        this.taskDecorator = taskDecorator;
+    }
+
+    @PostConstruct
+    private void init() {
+        for (Map.Entry<String, MultiThreadPoolProperties.ThreadPoolProperties> entry
+                : multiThreadPoolProperties.getProperties().entrySet()) {
+            String beanName = entry.getKey();
+            MultiThreadPoolProperties.ThreadPoolProperties properties = entry.getValue();
+            log.info("Init threadpool '{}'", beanName);
+            ThreadPoolTaskExecutor executor = ManualRegistBeanUtil
+                    .registerBean((ConfigurableApplicationContext) applicationContext, beanName, ThreadPoolTaskExecutor.class);
+            initExecutorProperties(executor, properties);
+            threadPoolTaskExecutorCache.put(beanName, executor);
+        }
+    }
+
+    private void initExecutorProperties(ThreadPoolTaskExecutor taskExecutor, MultiThreadPoolProperties.ThreadPoolProperties properties) {
+        PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
+        map.from(properties::getQueueCapacity).to(taskExecutor::setQueueCapacity);
+        map.from(properties::getCoreSize).to(taskExecutor::setCorePoolSize);
+        map.from(properties::getMaxSize).to(taskExecutor::setMaxPoolSize);
+        map.from(properties::getKeepAliveSeconds).asInt(Duration::getSeconds).to(taskExecutor::setKeepAliveSeconds);
+        map.from(properties::isAllowCoreThreadTimeout).to(taskExecutor::setAllowCoreThreadTimeOut);
+        map.from(properties::getBeanName).whenHasText().to(taskExecutor::setThreadNamePrefix);
+        map.from(taskDecorator::getIfUnique).to(taskExecutor::setTaskDecorator);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        if (threadPoolTaskExecutorCache.isEmpty()) {
+            return;
+        }
+
+        threadPoolTaskExecutorCache.forEach((beanName, threadPoolTaskExecutor) -> {
+            threadPoolTaskExecutor.shutdown();
+        });
+    }
+}
---
+++ src/main/java/io/github/xiaoxuetu/threadpool/MultiThreadPoolProperties.java
@@ -0,0 +1,86 @@
+package io.github.xiaoxuetu.threadpool;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@ConfigurationProperties(prefix = "spring.threadpools")
+public class MultiThreadPoolProperties {
+
+    /**
+     * 默认线程池名称
+     */
+    public static final String DEFAULT_THREAD_POOL_NAME = "default";
+
+    public static final ThreadPoolProperties DEFAULT_THREAD_POOL_PROPERTIES = new ThreadPoolProperties();
+
+    /**
+     * 线程池配置集合
+     */
+    private Map<String, ThreadPoolProperties> properties = new HashMap<>();
+
+    /**
+     * 线程池配置类
+     */
+    @Data
+    public static class ThreadPoolProperties {
+
+        /**
+         * 线程池名称，同时会作为线程池对前缀
+         */
+        private String beanName = DEFAULT_THREAD_POOL_NAME;
+
+        /**
+         * 线程池ShutDown时，是否等待任务完成
+         *
+         * 默认值: @see java.lang.Boolean#TRUE, 表示需要等待任务完成再关闭
+         */
+        private boolean waitForTasksToCompleteOnShutdown = Boolean.TRUE;
+
+        /**
+         * 线程池关闭等待时间
+         *
+         * 默认值: 30，表示等待任务完成时长为30秒
+         */
+        private Duration awaitTerminationSeconds = Duration.ofSeconds(30);
+
+        /**
+         * 核心线程数
+         *
+         * 默认值: 0，表示该线程池最少保留1个线程
+         */
+        private int coreSize = 1;
+
+        /**
+         * 池子最大线程数
+         *
+         * 默认值为CPU核心数量
+         */
+        private int maxSize = Runtime.getRuntime().availableProcessors();
+
+        /**
+         * 线程空转保留时长
+         *
+         * 默认值: 60，表示线程空转时长为60秒
+         */
+        private Duration keepAliveSeconds = Duration.ofSeconds(60);
+
+        /**
+         * 当线程空转超过保留时长时，是否允许释放
+         *
+         * 默认值：true，表示释放
+         */
+        private boolean allowCoreThreadTimeout = Boolean.FALSE;
+
+        /**
+         * 队列最大数量
+         *
+         * 默认值: @see java.lang.Interger#MAX_VALUE
+         */
+        private int queueCapacity = Integer.MAX_VALUE;
+    }
+}
